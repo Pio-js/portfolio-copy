42f320c4e821d52109f7bd17e9f5c26df8d33c88
Pio-js <pio.saija@web.de>
2022-09-11 18:05:52 +0200

    recaptcha now is working if you change language too

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,17 @@
+import { useRef, useEffect } from "react";
 import ProjectsList from "../components/ProjectsList";
 import './Projects.css';
 
 export default function Projects(props) {
 
+    const titleRef = useRef();
+
+    useEffect(() => {
+        titleRef.current.scrollIntoView({behavior: "smooth", block: "start"});
+    }, []);
+
     return (
-        <div id="projects">
+        <div ref={titleRef} id="projects">
             <ProjectsList data={props.data} />
         </div>
     )
